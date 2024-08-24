b1d2c74401da21dd99cb1d5f4307ba523176347e
Hitartha Saikia <hitarthas@gmail.com>
2024-08-24 07:51:49 +0530

    Updated

--- Rhubarb/lib/cmake/GTest/GTestConfig.cmake
+++ Rhubarb/lib/cmake/GTest/GTestConfig.cmake
@@ -1,33 +1,3 @@
-
-####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
-####### Any changes to this file will be overwritten by the next CMake run ####
-####### The input file was Config.cmake.in                            ########
-
-get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
-
-macro(set_and_check _var _file)
-  set(${_var} "${_file}")
-  if(NOT EXISTS "${_file}")
-    message(FATAL_ERROR "File or directory ${_file} referenced by variable ${_var} does not exist !")
-  endif()
-endmacro()
-
-macro(check_required_components _NAME)
-  foreach(comp ${${_NAME}_FIND_COMPONENTS})
-    if(NOT ${_NAME}_${comp}_FOUND)
-      if(${_NAME}_FIND_REQUIRED_${comp})
-        set(${_NAME}_FOUND FALSE)
-      endif()
-    endif()
-  endforeach()
-endmacro()
-
-####################################################################################
-include(CMakeFindDependencyMacro)
-if (ON)
-  set(THREADS_PREFER_PTHREAD_FLAG )
-  find_dependency(Threads)
-endif()
-
-include("${CMAKE_CURRENT_LIST_DIR}/GTestTargets.cmake")
-check_required_components("")
+version https://git-lfs.github.com/spec/v1
+oid sha256:e4c893b3e99e6309deb1d155c4bafd3faa7c8be343b08a0c7065f7eed0c5506c
+size 1073
